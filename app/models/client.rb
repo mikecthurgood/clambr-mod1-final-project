a12f2187c6e5424f7967564db8b16ad201523c9d
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-09 14:27:40 +0100

    client methods created

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,36 @@
 class Client < ActiveRecord::Base
     has_many :sessions
     has_many :walls, through: :sessions
+
+    def find_a_wall(name)
+        Wall.find_by(name: name) 
+    end
+    
+    def book_a_session(wall)
+        Session.create(wall_id: wall.id, client_id: self.id)
+    end
+
+    def cancel_last_session(wall)
+        Session.find(self.id).first.destroy
+    end
+
+    def which_walls?
+        self.walls.map(&:name)
+    end
+
+    def which_trainers?
+        self.trainers.map(&:name)
+    end
+
+    def number_of_sessions?
+        self.all_walls_attended.length
+    end
+
+    def self.create_user(name, grade)
+        Client.create(name: name, grade: grade)
+    end
+
+        
+
 end
     
