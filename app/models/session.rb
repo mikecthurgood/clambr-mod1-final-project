bb61d538b8885767a826d25c89897a2923a054df
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-09 14:27:54 +0100

    not working code

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,20 +3,20 @@ class Session < ActiveRecord::Base
     belongs_to :wall #ask mani
     belongs_to :trainer
 
-    def self.book
-        puts "Hi, who are you booking a session for?"
-            username_input = gets.chomp
-            user = find_or_create_by(name: username)
-            username = user.name
-        puts "Welcome #{username}! Which wall would you like to book a session at?"
-            desired_wall = gets.chomp
-            wall = find_a_wall(desired_wall)
-        if wall
-            Session.create(wall_id: wall.id, client_id: user.id)
-        else
-            puts "Sorry, that wall doesn't exist. Please try again"
-        end
-    end
+    # def self.book
+    #     puts "Hi, who are you booking a session for?"
+    #         username_input = gets.chomp
+    #         user = find_or_create_by(name: username)
+    #         username = user.name
+    #     puts "Welcome #{username}! Which wall would you like to book a session at?"
+    #         desired_wall = gets.chomp
+    #         wall = find_a_wall(desired_wall)
+    #     if wall
+    #         Session.create(wall_id: wall.id, client_id: user.id)
+    #     else
+    #         puts "Sorry, that wall doesn't exist. Please try again"
+    #     end
+    # end
 
 end
     
