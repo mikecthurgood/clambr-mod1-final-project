bddd077520b5f55a363da2500104bd7bd97c3c59
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-09 14:48:30 +0100

    demo code working

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,20 +3,21 @@ class Session < ActiveRecord::Base
     belongs_to :wall #ask mani
     belongs_to :trainer
 
-    # def self.book
-    #     puts "Hi, who are you booking a session for?"
-    #         username_input = gets.chomp
-    #         user = find_or_create_by(name: username)
-    #         username = user.name
-    #     puts "Welcome #{username}! Which wall would you like to book a session at?"
-    #         desired_wall = gets.chomp
-    #         wall = find_a_wall(desired_wall)
-    #     if wall
-    #         Session.create(wall_id: wall.id, client_id: user.id)
-    #     else
-    #         puts "Sorry, that wall doesn't exist. Please try again"
-    #     end
-    # end
+    def self.book
+        puts "Hi, who are you booking a session for?"
+            username_input = STDIN.gets.chomp
+            user = Client.find_or_create_by(name: username_input)
+            username = user.name
+        puts "Welcome #{username}! Which wall would you like to book a session at?"
+            desired_wall = STDIN.gets.chomp
+            wall = Wall.find_by(name: desired_wall)
+        if wall
+            coach = Trainer.all.sample
+            Session.create(wall_id: wall.id, client_id: user.id, trainer_id: coach)
+        else
+            puts "Sorry, that wall doesn't exist. Please try again"
+        end
+    end
 
 end
     
