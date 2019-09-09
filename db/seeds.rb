6eca85b126ad42d0b6a7f6f016c1597d680181ee
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-09 14:28:08 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,30 @@
-Wall.create(name: "Stronghold", location: "Tottenham Hale")
-Wall.create(name: "Harrowall", location: "Harrow")
-Wall.create(name: "The Climbing Hangar", location: "Parsons Green")
-Wall.create(name: "Mile End Climbing Centre", location: "Mile End")
-Wall.create(name: "White Spider Climbing", location: "Tolworth")
-Wall.create(name: "The Depot Climbing Centre", location: "Manchester")
-Wall.create(name: "The Works Climbing Centre", location: "Sheffield")
-Wall.create(name: "Hackney Wick Boulder Project", location: "Hackney Wick")
-Wall.create(name: "Colchester Climbing Project", location: "Colchester")
-Wall.create(name: "The Arch Climbing Wall", location: "Bermondsey")
+# Wall.create(name: "Stronghold", location: "Tottenham Hale")
+# Wall.create(name: "Harrowall", location: "Harrow")
+# Wall.create(name: "The Climbing Hangar", location: "Parsons Green")
+# Wall.create(name: "Mile End Climbing Centre", location: "Mile End")
+# Wall.create(name: "White Spider Climbing", location: "Tolworth")
+# Wall.create(name: "The Depot Climbing Centre", location: "Manchester")
+# Wall.create(name: "The Works Climbing Centre", location: "Sheffield")
+# Wall.create(name: "Hackney Wick Boulder Project", location: "Hackney Wick")
+# Wall.create(name: "Colchester Climbing Project", location: "Colchester")
+# Wall.create(name: "The Arch Climbing Wall", location: "Bermondsey")
 
-100.times do
-    name = Faker::Name.unique.name
-    grade = rand(1..10)
-    Client.create(name: name, grade: grade )
-end
+# 100.times do
+#     name = Faker::Name.unique.name
+#     grade = rand(1..10)
+#     Client.create(name: name, grade: grade )
+# end
 
-20.times do
-    name = Faker::Name.unique.name
-    grade = rand(8..15)
-    Trainer.create(name: name, grade: grade)
+# 20.times do
+#     name = Faker::Name.unique.name
+#     grade = rand(8..15)
+#     Trainer.create(name: name, grade: grade)
+# end
+
+10.times do
+    wall_id = rand(1..10)
+    client_id = rand(1..100)
+    Session.create(wall_id: wall_id, client_id: client_id)
 end
 
 
